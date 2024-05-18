198ff28a2e457d813636c5ddc55758e89914e047
iRishabhSingh <rishab18011@gmail.com>
2024-05-18 18:17:10 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="p-8 sm:px-16 md:px-48">
+      <header className="flex justify-between p-2 max-w-[1024px] m-auto">
+        <h5 className="text-lg font-semibold">Course builder</h5>
+        <button>Add</button>
+      </header>
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
